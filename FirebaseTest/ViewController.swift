15d5530fdace8f2eb5994c733191e53bac840eff
星野恵瑠 <el.hoshino@me.com>
2019-04-15 16:21:54 +0900

    Xcode init project

---
+++ FirebaseTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseTest
+//
+//  Created by 史 翔新 on 2019/04/15.
+//  Copyright © 2019 Crazism.net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
